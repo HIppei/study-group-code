eed1bc9bf5b9379dd109ee4087cbff5c031a4614
Ippei Horiba <ip.horiba@gmail.com>
2022-05-10 00:36:59 +0900

    code refactoring lesson

---
+++ sample.js
@@ -0,0 +1,8 @@
+const buttonFunc = () => {
+  var chkBoxFlag = document.getElementById("checkId").checked;
+  if (chkBoxFlag == true) {
+    window.alert("true");
+  } else if (chkBoxFlag == false) {
+    window.alert("false");
+  }
+};
